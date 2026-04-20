558798c36d586c06ebca55e8f51ad78bb83bbe66
eggman79 <eggman79@protonmail.com>
2026-04-20 09:55:30 +0200

    fix

--- ext.asm
+++ ext.asm
@@ -10,13 +10,16 @@ section .text
 
 extern heap_ptr
 extern stack_ptr
+extern print_int64
+
 extern instr_ptr;
 global test
 global add
 global run
 global run_exit
 global push
-global print
+global iff
+global printi8
 
 run:
   mov r12, qword[rel stack_ptr]
@@ -36,6 +39,32 @@ push:
 add:
   mov rcx, qword[r12 - 8] 
   add qword[r12 - 16], rcx
+  sub r12, 8
+  inc r13
+  next_instr
+
+iff:
+  sub r12, 8
+  mov rcx, qword[r12]
+  test rcx, rcx
+  jz if_false
+  mov rcx, qword[r13 + 1]
+  add r13, rcx
+  jmp if_exit
+if_false:
+  add r13, 9
+if_exit:
+  next_instr
+
+printi8:
+  push r12
+  push r13
+  push r14
+  mov rdi, qword[r12 - 8]
+  call print_int64
+  pop r14
+  pop r13
+  pop r12
   sub r12, 8
   inc r13
   next_instr
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <cstdint>
+#include <type_traits>
 #include <array>
 #include <vector>
 #include <iostream>
@@ -13,11 +14,13 @@ std::array<uint8_t, 1024> code;
 void* code_ptr = (void*)code.data();
 
 extern "C" void* push;
+extern "C" void* printi8;
 extern "C" void* add;
+extern "C" void* iff;
 extern "C" void* run_exit;
 extern "C" void run(void* code);
 
-std::array<void*, 4> instr = {&run_exit, &push, nullptr, &add};
+constexpr std::array<void*, 6> instr = {&run_exit, &push, nullptr, &add, &printi8, &iff};
 void* instr_ptr = (void*)instr.data();
 
 enum class Instr: uint8_t {
@@ -25,31 +28,65 @@ enum class Instr: uint8_t {
   Push,
   Pop,
   Add,
+  Print,
+  If,
 };
+
+namespace detail {
+    // Non-enum: identity type
+    template<typename T, bool IsEnum = std::is_enum_v<T>>
+    struct underlying_type { using type = T; };
+    
+    // Enum: std::underlying_type
+    template<typename T>
+    struct underlying_type<T, true> : std::underlying_type<T> {};
+}
+
+template<typename T>
+using integral_type_t = typename detail::underlying_type<T>::type;
 
 struct Code {
   std::vector<uint8_t> code;
 
   template <typename T>
-  void append(T value) {
-    if constexpr (sizeof(value) == 1) {
-      code.emplace_back(value);
-    } else {
-      constexpr auto value_size = sizeof(value);
-      code.resize(code.size() + value_size);
-      *(T*)&code[code.size() - value_size] = value;
+    void append(T value) {
+      if constexpr (sizeof(value) == 1) {
+        code.emplace_back((integral_type_t<T>)value);
+      } else {
+        constexpr auto value_size = sizeof(value);
+        code.resize(code.size() + value_size);
+        *(integral_type_t<T>*)&code[code.size() - value_size] = value;
+      }
     }
+
+  void commit() {
+
   }
 };
 
+extern "C" void print_int64(int64_t value) {
+  std::cout << value << std::endl;
+}
+
 int main() {
   Code code;
-  code.append((uint8_t)1);
-  code.append((uint64_t)12);
-  code.append((uint8_t)1);
-  code.append((uint64_t)13);
-  code.append((uint8_t)3);
-  code.append((uint8_t)0);
+  code.append(Instr::Push);
+  code.append(1ULL);
+  code.append(Instr::If);
+  const auto off1 = code.code.size();
+
+  code.append(0ULL);
+  code.append(Instr::Push);
+  code.append(100ULL);
+  code.append(Instr::Print);
+
+  const auto off2 = code.code.size();
+  *(uintptr_t*)&code.code[off1] = off2 - off1 + 1;
+
+  code.append(Instr::Push);
+  code.append(101ULL);
+  code.append(Instr::Print);
+  code.append(Instr::Exit);
   run(code.code.data());
   return 0;
 }
