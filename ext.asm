9c6594af6e1e988ff4c48e418294d66fe6281317
eggman79 <eggman79@protonmail.com>
2026-04-21 14:22:58 +0200

    fix

--- ext.asm
+++ ext.asm
@@ -10,6 +10,7 @@ section .text
 
 extern heap_ptr
 extern stack_ptr
+extern call_stack_ptr
 extern print_int64
 extern instr_ptr;
 
@@ -17,14 +18,18 @@ global run
 global run_exit
 
 global add_instr
+global sub_instr
 global jmp_instr
 global push_instr
+global pop_instr
 global if_instr
 global call_instr
 global ret_instr
+global dup_instr
 global printi8
 
 run:
+  mov r11, qword[rel call_stack_ptr]
   mov r12, qword[rel stack_ptr]
   mov r13, rsi
   mov r14, qword[rel instr_ptr]
@@ -39,9 +44,20 @@ push_instr:
   add r13, 9
   next_instr
 
+pop_instr:
+  sub r12, 8
+  next_instr
+
 add_instr:
   mov rcx, qword[r12 - 8] 
   add qword[r12 - 16], rcx
+  sub r12, 8
+  inc r13
+  next_instr
+
+sub_instr:
+  mov rcx, qword[r12 - 8] 
+  sub qword[r12 - 16], rcx
   sub r12, 8
   inc r13
   next_instr
@@ -67,15 +83,26 @@ jmp_instr:
 call_instr:
   mov rcx, r13
   inc rcx
-  mov r13, qword[r12 - 8]
-  mov qword[r12 - 8], rcx
+  sub r12, 8
+  mov r13, qword[r12]
+  mov qword[r11], rcx
+  add r11, 8
   next_instr
 
 ret_instr:
-  mov r13, qword[r12 - 8]
+  sub r11, 8
+  mov r13, qword[r11]
+  next_instr
+
+dup_instr:
+  inc r13
+  mov rcx, qword[r12 - 8]
+  mov qword[r12], rcx
+  add r12, 8
   next_instr
 
 printi8:
+  push r11
   push r12
   push r13
   push r14
@@ -84,6 +111,7 @@ printi8:
   pop r14
   pop r13
   pop r12
+  pop r11
   sub r12, 8
   inc r13
   next_instr
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <cstdint>
+#include <cinttypes>
 #include <limits>
 #include <type_traits>
 #include <array>
@@ -15,23 +16,31 @@ void* heap_ptr = (void*)heap.data();
 std::array<uint8_t, 1024> stack;
 void* stack_ptr = (void*)stack.data();
 
+std::array<uint8_t, 1024> call_stack;
+void* call_stack_ptr = (void*)call_stack.data();
+
 std::array<uint8_t, 1024> code;
 void* code_ptr = (void*)code.data();
 
 extern "C" void* push_instr;
+extern "C" void* pop_instr;
 extern "C" void* printi8;
 extern "C" void* add_instr;
+extern "C" void* sub_instr;
 extern "C" void* jmp_instr;
 extern "C" void* if_instr;
 extern "C" void* call_instr;
 extern "C" void* ret_instr;
+extern "C" void* dup_instr;
 extern "C" void* run_exit;
 extern "C" void run(void* code);
 
-constexpr std::array<void*, 9> instr = {
-  &run_exit, &push_instr, nullptr, 
-  &add_instr, &printi8, &if_instr, &jmp_instr, 
-  &call_instr, &ret_instr};
+constexpr std::array<void*, 11> instr = {
+  &run_exit, &push_instr, &pop_instr,
+  &add_instr, &sub_instr,
+  &printi8, &if_instr, &jmp_instr, 
+  &call_instr, &ret_instr, &dup_instr};
+
 void* instr_ptr = (void*)instr.data();
 
 enum class Instr: uint8_t {
@@ -39,11 +48,13 @@ enum class Instr: uint8_t {
   Push,
   Pop,
   Add,
+  Sub,
   Print,
   If,
   Jump,
   Call,
   Ret,
+  Dup,
 };
 
 struct Class {
@@ -61,7 +72,6 @@ struct Class {
   PropIndex get_prop() const {
     return UndefinedIndex;    
   }
-
 };
 
 struct String : Class {
@@ -94,31 +104,73 @@ struct Code {
       *(integral_type_t<T>*)&code[code.size() - value_size] = value;
     }
   }
+
+  void update_call_addr(size_t call_addr, size_t method_addr) {
+    *(void**)&code[call_addr] = (void*)&code[method_addr];
+  }
+};
+
+struct Strings {
+  std::vector<std::string> items;
 };
 
 extern "C" void print_int64(int64_t value) {
-  std::cout << value << std::endl;
+  printf("%" PRId64 "\n", value);
 }
 
 int main() {
   Code code;
 
   code.append(Instr::Push);
-  code.append(100ULL);
-  code.append(Instr::Print);
+  code.append(1ULL);
   code.append(Instr::Push);
-  const auto call_offset = code.code.size();
+  code.append(2ULL);
+  code.append(Instr::Push);
+
+  const auto call_addr = code.code.size();
   code.append(0ULL);
   code.append(Instr::Call);
+  code.append(Instr::Dup);
+  code.append(Instr::Dup);
+  code.append(Instr::Print);
+  code.append(Instr::Print);
+  code.append(Instr::Print);
+code.append(Instr::Push);
+  code.append(10ULL);
+
+  code.append(Instr::Push);
+  code.append(10ULL);
+
+  code.append(Instr::Push);
+  const auto call_addr3 = code.code.size();
+
+  code.append(0ULL);
+  code.append(Instr::Call);
+
+  code.append(Instr::Push);
+  code.append(10ULL);
+  code.append(Instr::Push);
+
+  const auto call_addr2 = code.code.size();
+  code.append(0ULL);
+  code.append(Instr::Call);
+  code.append(Instr::Print);
   code.append(Instr::Exit);
 
-  const auto off = code.code.size();  
-  code.append(Instr::Push);
-  code.append(10000ULL);
-  code.append(Instr::Print);
+  const auto method_addr = code.code.size();
+  code.append(Instr::Add);
   code.append(Instr::Ret);
 
-  *(void**)&code.code[call_offset] = (void*)&code.code[off];
+
+  const auto method2_addr = code.code.size();
+
+  code.append(Instr::Add);
+  code.append(Instr::Ret);
+
+  code.update_call_addr(call_addr, method_addr);
+  code.update_call_addr(call_addr2, method_addr);
+  code.update_call_addr(call_addr3, method_addr);
+
   run(code.code.data());
 
   return 0;
